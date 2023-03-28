bd1cf6656c96d9f5d8dd3009642ea5d0c7b11ecb
bcd21 <96047538+bcd21@users.noreply.github.com>
2023-03-29 04:36:46 +0800

    Add files via upload

---
+++ Homey Solution/JS/script.js
@@ -0,0 +1,17 @@
+let mobileMenu = document.querySelector('.mobile-menu');
+
+mobileMenu.addEventListener('click', function(){
+	if(!document.querySelector('#line1').classList.contains('line_not-active')){
+		document.querySelector('#line1').classList.add('line_not-active');
+		document.querySelector('#line3').classList.add('line_not-active');
+		document.querySelector('#line2').style.rotate = '45deg';
+		document.querySelector('#line4').style.rotate = '-45deg';
+		document.querySelector('.header-nav').style.left = '0';
+	}else{
+		document.querySelector('#line1').classList.remove('line_not-active');
+		document.querySelector('#line3').classList.remove('line_not-active');
+		document.querySelector('#line2').style.rotate = '0deg';
+		document.querySelector('#line4').style.rotate = '0deg';
+		document.querySelector('.header-nav').style.left = '-100%';
+	}
+})
