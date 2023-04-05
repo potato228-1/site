55c3bb2fb424c2ce8a26a6b391516d1e5930898d
bcd21 <96047538+bcd21@users.noreply.github.com>
2023-04-05 23:03:06 +0800

    Add files via upload

--- The Art - Exhibit/Script/script.js
+++ The Art - Exhibit/Script/script.js
@@ -6,12 +6,12 @@ mobileMenu.addEventListener('click', function() {
         document.querySelector('.line3').classList.add('line_not-active');
         document.querySelector('.line2').style.rotate = '45deg';
         document.querySelector('.line4').style.rotate = '-45deg';
-        document.querySelector('.header-nav').style.left = '0';
+        document.querySelector('.header').style.left = '0';
     } else {
         document.querySelector('.line1').classList.remove('line_not-active');
         document.querySelector('.line3').classList.remove('line_not-active');
         document.querySelector('.line2').style.rotate = '0deg';
         document.querySelector('.line4').style.rotate = '0deg';
-        document.querySelector('.header-nav').style.left = '-100%';
+        document.querySelector('.header').style.left = '-100%';
     }
 })
